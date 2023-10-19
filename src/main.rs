8c5c37a47e816e36c35232e99391bfa11351a81d
Alexander Oh <oh.a@gmx.at>
2023-10-19 16:20:30 +0200

    prettify

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,6 @@ use git2::{ObjectType, Repository, TreeWalkMode, TreeWalkResult};
 use serde::Serialize;
 use tokio::runtime::Runtime;
 
-#[derive(Debug, Serialize, Clone)]
-struct Blame {
-    author: String,
-    lines: usize,
-}
-
-type Blames = Vec<Blame>;
-
-#[derive(Clone, Debug)]
-enum BlameMessage {
-    Blame(Blames),
-    Count(usize)
-}
-
 #[derive(Clone)]
 struct CancellationToken {
     sender: Arc<Mutex<bool>>,
@@ -36,74 +22,6 @@ impl CancellationToken {
     fn new() -> Self { Self { sender: Arc::new(Mutex::new(false)) } }
     fn cancel(&self) { *self.sender.lock().unwrap() = true; }
     fn is_cancelled(&self) -> bool { *self.sender.lock().unwrap() }
-}
-
-fn make_blame(repo: &Repository, fname: &Path) -> Result<Blames>
-{
-    let fblame = repo.blame_file(&fname, None)?;
-    let mut authors = HashMap::<String, usize>::new();
-
-    for blame_chunk in fblame.iter() {
-        let author = blame_chunk.final_signature().email().unwrap_or("unknown").to_string();
-        let lines = blame_chunk.lines_in_hunk();
-        let entry = authors.entry(author).or_insert(0);
-        *entry += lines;
-    }
-    Ok(hm_into_vec(&authors))
-}
-
-fn get_tree<F>(repo: &Repository, updater: &mut F) -> Result<usize>
-    where F: FnMut(&str) -> Result<()>
-{
-    let head = repo.head()?.peel_to_tree()?;
-    let mut cnt: usize = 0;
-
-    head.walk(TreeWalkMode::PreOrder, move |path, entry| {
-        if entry.kind() != Some(ObjectType::Blob) { return TreeWalkResult::Ok; }
-        let mut result = path.to_owned();
-        result.push_str(entry.name().expect("empty filename"));
-        cnt = cnt + 1; // why does += 1 not work?
-        let result = updater(&result);
-        if result.is_err() { TreeWalkResult::Abort } else { TreeWalkResult::Ok }
-    })?;
-
-    Ok(cnt)
-}
-
-fn blame_fold(mut hm: HashMap<String, usize>,  blame: Blames) -> HashMap<String, usize> {
-    for blame_chunk in blame {
-        let entry = hm.entry(blame_chunk.author).or_insert(0);
-        *entry += blame_chunk.lines;
-    }
-    hm
-}
-
-fn blame_f(hm: &mut HashMap<String, usize>,  blame: Blames)  {
-    for blame_chunk in blame {
-        let entry = hm.entry(blame_chunk.author).or_insert(0);
-        *entry += blame_chunk.lines;
-    }
-}
-
-fn hm_into_vec(authors: &HashMap<String, usize>) -> Blames {
-    let mut blames: Blames = authors.iter()
-        .map(|(x, y)| { Blame {author: x.to_owned(), lines: y.to_owned()} })
-        .collect();
-    blames.sort_by(|lhs, rhs| rhs.lines.cmp(&lhs.lines));
-    blames
-}
-
-fn single_threaded(path: &str) -> Result<Blames> {
-    let repo = Repository::open(path)?;
-    let mut files = Vec::new();
-    let _ = get_tree(&repo, &mut |path: &str| {
-        files.push(path.to_owned());
-        Ok(())
-    })?;
-    let result = files.iter()
-        .map(|f| { make_blame(&repo, Path::new(&f)).expect("unblamable") })
-        .fold(HashMap::new(), &blame_fold);
-    Ok(hm_into_vec(&result))
 }
 
 async fn retry<F, E, V>(mut f: F, mut attempts: u8, interval: Duration) -> Result<V, E>
@@ -125,6 +43,89 @@ async fn retry<F, E, V>(mut f: F, mut attempts: u8, interval: Duration) -> Resul
     }
 }
 
+#[derive(Debug, Serialize, Clone)]
+struct Blame {
+    author: String,
+    lines: usize,
+}
+
+type Blames = Vec<Blame>;
+type HBlames = HashMap<String, usize>;
+
+#[derive(Clone, Debug)]
+enum BlameMessage {
+    Blame(HBlames),
+    Count(usize)
+}
+fn blame_file(repo: &Repository, fname: &Path) -> Result<HBlames>
+{
+    let fblame = repo.blame_file(fname, None)?;
+    let mut authors = HashMap::<String, usize>::new();
+
+    for blame_chunk in fblame.iter() {
+        let author = blame_chunk.final_signature().email().unwrap_or("unknown").to_string();
+        let lines = blame_chunk.lines_in_hunk();
+        let entry = authors.entry(author).or_insert(0);
+        *entry += lines;
+    }
+    Ok(authors)
+}
+
+fn get_tree<F>(repo: &Repository, updater: &mut F) -> Result<usize>
+    where F: FnMut(&str) -> Result<()>
+{
+    let head = repo.head()?.peel_to_tree()?;
+    let mut cnt: usize = 0;
+
+    head.walk(TreeWalkMode::PreOrder, move |path, entry| {
+        if entry.kind() != Some(ObjectType::Blob) { return TreeWalkResult::Ok; }
+        let mut result = path.to_owned();
+        result.push_str(entry.name().expect("empty filename"));
+        cnt = cnt + 1; // why does += 1 not work?
+        let result = updater(&result);
+        if result.is_err() { TreeWalkResult::Abort } else { TreeWalkResult::Ok }
+    })?;
+
+    Ok(cnt)
+}
+
+fn blame_fold(mut hm: HBlames,  blame: HBlames) -> HBlames {
+    for blame_chunk in blame {
+        let entry = hm.entry(blame_chunk.0).or_insert(0);
+        *entry += blame_chunk.1;
+    }
+    hm
+}
+
+fn blame_acc(hm: &mut HashMap<String, usize>,  blame: HBlames)  {
+    for blame_chunk in blame {
+        let entry = hm.entry(blame_chunk.0).or_insert(0);
+        *entry += blame_chunk.1;
+    }
+}
+
+fn hm_into_vec(authors: &HBlames) -> Blames {
+    let mut blames: Blames = authors.iter()
+        .map(|(x, y)| { Blame {author: x.to_owned(), lines: y.to_owned()} })
+        .collect();
+    blames.sort_by(|lhs, rhs| rhs.lines.cmp(&lhs.lines));
+    blames
+}
+
+fn single_threaded(path: &str) -> Result<Blames> {
+    let repo = Repository::open(path)?;
+    let mut files = Vec::new();
+    let _ = get_tree(&repo, &mut |path: &str| {
+        files.push(path.to_owned());
+        Ok(())
+    })?;
+    let result = files.iter()
+        .map(|f| { blame_file(&repo, Path::new(&f)).expect("unblamable") })
+        .fold(HashMap::new(), &blame_fold);
+    Ok(hm_into_vec(&result))
+}
+
+
 fn multi_threaded(path: &str, workers: usize) ->  Result<Blames> {
     let rt = Runtime::new().unwrap();
     let (to_pool, for_pool) = bounded(100);
@@ -139,7 +140,6 @@ fn multi_threaded(path: &str, workers: usize) ->  Result<Blames> {
         rt.spawn(async move {
             let repo = Repository::open(path).unwrap();
             let mut update = |path: &str| -> Result<()> {
-                // println!("trying to send {:?}", path);
                 to_pool.send(path.to_owned())?;
                 Ok(())
             };
@@ -157,22 +157,18 @@ fn multi_threaded(path: &str, workers: usize) ->  Result<Blames> {
             let repo = Repository::open(path).unwrap();
             while !ct.is_cancelled() {
                 let m = for_pool.recv().unwrap();
-                let res = make_blame(&repo, Path::new(&m)).unwrap();
+                let res = blame_file(&repo, Path::new(&m)).unwrap();
                 let bm = BlameMessage::Blame(res);
                 let retry_result = retry(|| { to_acc.send(bm.clone()) }, 20, Duration::from_millis(1)).await;
                 if retry_result.is_err() {
                     println!("closing worker, retries exceeded\n");
                 }
-
             }
         });
     }
 
     {
-        let for_acc = for_acc.clone();
         let mut hm = HashMap::new();
-        let ct = ct.clone();
-        let to_print = to_print.clone();
         rt.spawn(async move {
             let mut count = 0;
             let mut c = None;
@@ -180,7 +176,7 @@ fn multi_threaded(path: &str, workers: usize) ->  Result<Blames> {
                 let msg = for_acc.recv().unwrap();
                 match msg {
                     BlameMessage::Count(i) => c = Some(i),
-                    BlameMessage::Blame(b) => blame_f(&mut hm, b)
+                    BlameMessage::Blame(b) => blame_acc(&mut hm, b)
                 }
                 count += 1;
             }
@@ -189,7 +185,6 @@ fn multi_threaded(path: &str, workers: usize) ->  Result<Blames> {
         });
     }
     rt.block_on(async {});
-
     Ok(for_print.recv().unwrap())
 }
 
@@ -204,6 +199,10 @@ struct Args {
 
     #[arg(short, long, default_value = ".")]
     path: String,
+
+    #[arg(short, long, default_value_t = 0)]
+    top: usize,
+
 }
 
 fn main() -> Result<()> {
@@ -214,15 +213,20 @@ fn main() -> Result<()> {
     }
     println!("using {} workers", args.workers);
 
-
     let blames = if args.single {
         single_threaded(&args.path)?
     } else {
         multi_threaded(&args.path, args.workers)?
     };
     let mut wtr = csv::Writer::from_writer(io::stdout());
-    for b in blames {
-        wtr.serialize(b)?;
+    if args.top == 0 {
+        for b in blames {
+            wtr.serialize(b)?;
+        }
+    } else {
+        for i in 0..args.top {
+            wtr.serialize(blames.get(i))?;
+        }
     }
     Ok(())
 }
